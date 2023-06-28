e8b7e667ee21b76eeb4a6b2115f9aa4e0de4245c
ariki foster <ariki@student.dev-island.org>
2023-06-27 17:03:49 -1000

    updated code

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function (str, times) {
+  if (times < 0) {
+    return "ERROR";
+  }
 
+  let resultstr = "";
+  for (i = 0; i < times; i++) {
+    resultstr += str;
+  }
+  console.log(resultstr);
+  return resultstr;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function (collection, ...args) {
+  const newArray = [];
+  for (let i = 0; i < collection.length; i++) {
+    if (!args.includes(collection[i])) {
+      newArray.push(collection[i]);
+    }
+  }
 
+  return newArray;
 };
 
 // Do not edit below this line
