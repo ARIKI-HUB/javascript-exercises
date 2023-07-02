1f3e86845ad27617d118f1e463352af0c97e75bc
ariki foster <ariki@student.dev-island.org>
2023-07-01 16:24:54 -1000

    04_removefromarray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -9,5 +9,10 @@ const removeFromArray = function (collection, ...args) {
   return newArray;
 };
 
+//let names = ["bob", "teddy", "bear"];
+//for (let i = 0; i < names.length; i++) {
+// console.log(names[i]);
+//}
+
 // Do not edit below this line
 module.exports = removeFromArray;
