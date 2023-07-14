61b8f10897f23f783bd58c3f5a65b0d79f08120d
ariki foster <ariki@student.dev-island.org>
2023-07-13 20:36:43 -1000

    javascript-updated-code

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,11 @@
-const leapYears = function() {
-
+const leapYears = function (years) {
+  function leapYear(years) {
+    if (years % 4 === 0 && (years % 100 !== 0 || years % 400 === 0)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,12 @@
-const convertToCelsius = function() {
-};
+const convertToCelsius = function () {};
 
-const convertToFahrenheit = function() {
-};
+const convertToFahrenheit = function () {};
+// celcius to farinheit and vice versa
+//conversion from celcius to farinheit is (0.c x 9/5) + 32 = 32.F
+//make it rounded so its only one decimal place
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
